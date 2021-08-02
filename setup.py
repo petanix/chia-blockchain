86a56831604236c09ac17686767d1c1bd72eb157
PetaniX Administrator <88309256+petanix-admin@users.noreply.github.com>
2021-08-02 16:22:31 +0700

    fix: setup.py git package error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dependencies = [
     "chiabip158==1.0",  # bip158-style wallet filters
     "chiapos==1.0.4",  # proof of space
     "clvm==0.9.7",
-    "git+https://github.com/petanix/clvm_rs@main",
+    "clvm_rs @ git+https://github.com/petanix/clvm_rs@main#egg=clvm_rs",
     "clvm_tools==0.4.3",
     "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
